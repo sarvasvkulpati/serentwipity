5e93e09a736a94bb64a7d13557bd55e4c5f65465
Sarvasv Kulpati <sarvasvkulpati@gmail.com>
2021-09-21 06:15:11 +0000

    prod ready

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cookieParser())
 const tw = new LoginWithTwitter({
   consumerKey: process.env.TWITTER_API_KEY,
   consumerSecret: process.env.TWITTER_API_SECRET,
-  callbackUrl: 'https://ScaredQuaintExponent.sarv.repl.co/twitter/callback'
+  callbackUrl: 'https://serentwipity.sarv.repl.co/twitter/callback'
 })
 
 
@@ -39,6 +39,7 @@ app.get('/twitter', (req, res) => {
     
     // Save the OAuth token secret for use in your /twitter/callback route
     req.session.tokenSecret = tokenSecret
+    console.log('tokenSecret', tokenSecret)
     
     // Redirect to the /twitter/callback route, with the OAuth responses as query params
     res.redirect(url)
@@ -56,15 +57,10 @@ app.get('/twitter/callback',  (req, res) => {
       return res.status(500).send(err)
     }
     
-    // Delete the tokenSecret securely
-    // delete req.session.tokenSecret
-
-
-    //check if user exists in database
-    
     let dbUser = await db.get(user.userId)
-
     if(dbUser) {
+
+      console.log('user exists')
    
       let secret = user.userTokenSecret
 
@@ -78,10 +74,11 @@ app.get('/twitter/callback',  (req, res) => {
           path: "/"
         })
 
-    res.redirect('/')
+      res.redirect('/')
     } else {
+
+    console.log('creating user')
       
-  
     // The user object contains 4 key/value pairs, which
     // you should store and use as you need, e.g. with your
     // own calls to Twitter's API, or a Twitter API module
@@ -94,78 +91,7 @@ app.get('/twitter/callback',  (req, res) => {
     // }
     req.session.user = user
 
-    
-
-
-    const twitterClient = new TwitterClient({
-      apiKey: process.env.TWITTER_API_KEY,
-      apiSecret: process.env.TWITTER_API_SECRET,
-      accessToken: user.userToken,
-      accessTokenSecret: user.userTokenSecret,
-    });
-
-
-    // do stuff with userToken 
-
-      //get ids of followers
-
-    let followerIds = await twitterClient.accountsAndUsers.followersIds({user_id: user.userId})
-
- 
-
-    let followerIdsBatches = chunk(followerIds['ids'], 100);
-
-    
-
-    for (let followerIdsBatch of followerIdsBatches) {
-
-     
-        let followerDataBatch = await twitterClient.accountsAndUsers.usersLookup({user_id : followerIdsBatch.join()})
-
-        
-
-
-          for(let followerData of followerDataBatch ) {
-
-              console.log(followerData, typeof(followerData))
-            let {
-              id_str: key, 
-              name, 
-              screen_name, 
-              location, 
-              description, 
-              url, 
-              followers_count,
-              friends_count,
-              following,
-              profile_image_url_https,
-              verified
-            } = followerData
-
-
-            let follower = {
-              key, 
-              name, 
-              screen_name, 
-              location, 
-              description, 
-              url, 
-              followers_count,
-              friends_count,
-              following,
-              profile_image_url_https,
-              verified
-            }
-
-
-
-            console.log(follower)
-
-
-            await followerBase.put({...follower, following: user.userId})
-      }
-    }
-
+    putFollowersInDb(user.userToken, user.userTokenSecret, user.userId)
 
     user = {
       key: user.userId,
@@ -173,27 +99,74 @@ app.get('/twitter/callback',  (req, res) => {
       userToken: user.userToken,
       userTokenSecret: user.userTokenSecret
     }
+
     db.put(user);
     
-    // Redirect to whatever route that can handle your new Twitter login user details!
-    // res.json(user)
-    
 
-    //res.cookie user
     res.cookie('auth', JSON.stringify({userToken: user.userToken, userTokenSecret: user.userTokenSecret, userId: user.key}), { expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 90),
     path: "/"})
 
 
     res.redirect('/');
     }
-    
-    
   });
 });
 
 
-let getFollowers = (userToken, userTokenSecret ) => {
+let putFollowersInDb = async (userToken, userTokenSecret, userId) => {
+
+
+  const twitterClient = new TwitterClient({
+      apiKey: process.env.TWITTER_API_KEY,
+      apiSecret: process.env.TWITTER_API_SECRET,
+      accessToken: userToken,
+      accessTokenSecret: userTokenSecret,
+    });
+
   
+  let followerIds = await twitterClient.accountsAndUsers.followersIds({user_id: userId})
+
+  let followerIdsBatches = chunk(followerIds['ids'], 100);
+  for (let followerIdsBatch of followerIdsBatches) {
+
+    let followerDataBatch = await twitterClient.accountsAndUsers.usersLookup({user_id : followerIdsBatch.join()})
+
+    for(let followerData of followerDataBatch ) {
+      console.log(followerData, typeof(followerData))
+
+      let {
+        id_str: key, 
+        name, 
+        screen_name, 
+        location, 
+        description, 
+        url, 
+        followers_count,
+        friends_count,
+        following,
+        profile_image_url_https,
+        verified
+      } = followerData
+
+      let follower = {
+        key, 
+        name, 
+        screen_name, 
+        location, 
+        description, 
+        url, 
+        followers_count,
+        friends_count,
+        following,
+        profile_image_url_https,
+        verified
+      }
+
+      console.log(follower)
+      await followerBase.put({...follower, following: userId}) 
+    }
+  }
+
 }
 
 const chunk = (arr, size) =>
--- public/script.js
+++ public/script.js
@@ -1,1 +1,71 @@
+let idx = 0;
+let data = [
+  {
+    'image': 'https://pbs.twimg.com/profile_images/1432725978120220672/uxZUTafa.jpg',
+    'name': 'visa is writing... ✍🏾📖', 
+    'description': 'Simple yet radical: focus your time + energy on what you want to see more of. 🌍💪🏾❤️🔥⚡️ buy my ebook FRIENDLY AMBITIOUS NERD https://t.co/2IBKmkd3kT', 
+    'location': '📍 🇸🇬, 🌏, 🌌', 
+    'url': '🔗 https://t.co/Mn4VLsT2Yk',
+    'followers': '33936', 
+    'following': '1620',
+    'profileUrl': 'https://twitter.com/visakanv',
+    
+  },
+  {
+    'image': 'https://pbs.twimg.com/profile_images/1428528008974016515/gAm8YPHM.jpg',
+    'name': 'tyler🫐🍄🐇🕸', 
+    'description': 'making things for making things @replit', 
+    'location': '📍NYC', 
+    'url': '🔗 https://t.co/aUlgM32LpG',
+    'followers': '2010', 
+    'following': '998',
+    'profileUrl': 'https://twitter.com/tylerangert',
+    
+  }
+]
 
+
+
+let image = document.getElementById('image');
+let name = document.getElementById('name');
+let description  = document.getElementById('description')
+let place = document.getElementById('location')
+let url = document.getElementById('url')
+let followers = document.getElementById('followers')
+let following = document.getElementById('following')
+let profileUrl = document.getElementById('profileUrl')
+
+
+
+
+let refresh = () => {
+
+  console.log('refreshing', idx)
+  if (idx == data.length - 1) {
+    console.log('hi')
+    idx = 0
+  } else {
+     console.log('hierer')
+    idx += 1
+  }
+
+
+  image.src = data[idx].image
+name.innerText = data[idx].name
+description.innerText = data[idx].description
+place.innerText  = data[idx].location
+url.innerText  = data[idx].url
+url.href = data[idx].url
+followers.innerText  = data[idx].followers
+following.innerText  = data[idx].following
+profileUrl.href  = data[idx].profileUrl
+
+
+
+}
+
+refresh()
+
+
+let refreshButton = document.getElementById('landingRefresh')
+refreshButton.addEventListener('click', refresh)
